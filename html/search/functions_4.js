90c466e08e10bef4839f9e630332a093f2cca856
kanna <86209224+USK253@users.noreply.github.com>
2021-07-30 17:41:43 +0200

    Delete html directory

--- html/search/functions_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['main_36',['main',['../namespacego__to__point.html#ab92bd6db1ec323a4d795ed6cb40aebe1',1,'go_to_point.main()'],['../namespaceuser__interface.html#ac26bdb296b6776907b72c17ce5a1b24a',1,'user_interface.main()']]]
-];
--- html/search/variables_5.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['state_5f_45',['state_',['../namespacego__to__point.html#aa2d0ef255bcf5d5faf9ca2764730fb17',1,'go_to_point']]]
-];
